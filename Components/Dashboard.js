673db388afadb0899489224d0ba68365db30b28f
blackdiamand <34006971+blackdiamand@users.noreply.github.com>
2024-02-17 16:57:51 -0800

    bank

--- Components/Dashboard.js
+++ Components/Dashboard.js
@@ -16,7 +16,43 @@ function Dashboard({ navigation}) {
       />
       <Button
         title="All Questions"
-        onPress={() => navigation.navigate('Questions')}
+        onPress={() => {
+            let obj = pointsContext;
+            obj.bankState = "all";
+            setPointsContext(obj);
+            navigation.navigate('Questions')}}
+      />
+      <Button
+        title="Principles of American Democracy"
+        onPress={() => {
+          let obj = pointsContext;
+          obj.bankState = "Principles";
+          setPointsContext(obj);
+          navigation.navigate('Questions')}}
+      />
+      <Button
+        title="System of Government"
+        onPress={() => navigation.navigate('System of Government')}
+      />
+      <Button
+        title="Rights and Responsibilities"
+        onPress={() => navigation.navigate('Rights and Responsibilities')}
+      />
+      <Button
+        title="Colonial Period History"
+        onPress={() => navigation.navigate('Colonial Period History')}
+      />
+      <Button
+        title="History"
+        onPress={() => navigation.navigate('History')}
+      />
+      <Button
+        title="Geography"
+        onPress={() => navigation.navigate('Geography')}
+      />
+      <Button
+        title="Holidays"
+        onPress={() => navigation.navigate('Holidays')}
       />
       {pointsContext.wrongAnswers.length > 0 &&
         <Button
--- Components/Question.js
+++ Components/Question.js
@@ -9,10 +9,23 @@ import QuestionButtons from "./QuestionButtons";
 function QuestionScreen({ navigation}) {
   const {questionContext, setQuestionContext} = useContext(QuestionContext);
   const {pointsContext, setPointsContext} = useContext(GlobalContext);
+  let questionBankState = pointsContext.bankState;
+  function bank() {
+    if (questionBankState === "all") {
+      return questionBank[Math.floor(Math.random() * 200)];
+    }
+    if (questionBankState === "Principles"){
+      let newBank = questionBank.filter((question) => question['category'] === "A: Principles of American Democracy");
+      return newBank[Math.floor(Math.random() * newBank.length)];
+    }
+    else{
+      return questionBank[Math.floor(Math.random() * 200)];
+    }
+  }
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Question: {questionContext['question']} </Text>
-      <QuestionButtons question={questionContext} newQuestion = {questionBank[Math.floor(Math.random() * 200)]} />
+      <QuestionButtons question={questionContext} newQuestion = {bank()} />
       <Text>Points: {pointsContext.score}</Text>
     </View>
   );
